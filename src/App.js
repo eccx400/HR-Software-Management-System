d832a88323ab5cf73b5c416e2068f34b2f30abea
eccx400 <echeng@scu.edu>
2021-07-28 16:24:53 -0700

    Frontend Finish

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
-import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify, { Predictions } from 'aws-amplify';
-import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+import React, { useState } from "react";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Jumbotron,
+  Container,
+} from "reactstrap";
+import { Card, CardText, CardBody, CardTitle } from "reactstrap";
+import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import Amplify, { Predictions } from "aws-amplify";
+import { AmazonAIPredictionsProvider } from "@aws-amplify/predictions";
 
-import awsconfig from './aws-exports';
+import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 function App() {
-  const [response, setResponse] = useState("Sentiment Results");
-  const [textToInterpret, setTextToInterpret] = useState("write some text here to interpret");
+  const [response, setResponse] = useState("");
+  const [textToInterpret, setTextToInterpret] = useState(
+    "Please enter your suggestions here"
+  );
 
   function interpretFromPredictions() {
     Predictions.interpret({
@@ -19,10 +31,11 @@ function App() {
         source: {
           text: textToInterpret,
         },
-        type: "ALL"
-      }
-    }).then(result => setResponse(JSON.stringify(result, null, 2)))
-      .catch(err => setResponse(JSON.stringify(err, null, 2)))
+        type: "ALL",
+      },
+    })
+      .then((result) => setResponse(JSON.stringify(result, null, 2)))
+      .catch((err) => setResponse(JSON.stringify(err, null, 2)));
   }
 
   function setText(event) {
@@ -31,13 +44,52 @@ function App() {
 
   return (
     <div className="Text">
-      <div>
-        <h1>Text interpretation</h1>
-        <input value={textToInterpret} onChange={setText}></input>
-        <button onClick={interpretFromPredictions}>Interpret Sentiment</button>
-        <p>{response}</p>
+      <div class="container-full-bg">
+        <Jumbotron fluid>
+          <Container className="themed-container" fluid="md">
+            <Card>
+              <CardTitle>
+                <h1 className="display-3">Employee Sentiment Analysis</h1>
+              </CardTitle>
+              <br />
+              <CardBody>
+                <Form>
+                  <FormGroup>
+                    <Label for="exampleName">Name</Label>
+                    <Input type="name" name="name" id="name" placeholder="John Doe" />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="exampleEmail">Email</Label>
+                    <Input type="email" name="email" id="email" placeholder="johndoe@sjsu.edu" />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="exampleEmail">Suggestions</Label>
+                    <Input
+                      addonType="append"
+                      type="textarea"
+                      value={textToInterpret}
+                      onChange={setText}
+                    />
+                    <br/>
+                    <Button
+                      className="btn btn-secondary"
+                      onClick={interpretFromPredictions}
+                      size="lg"
+                    >
+                      Submit
+                    </Button>
+                  </FormGroup>
+                </Form>
+              </CardBody>
+              <br />
+              <CardText>
+                <h2 classname="text-center">{response}</h2>
+              </CardText>
+              <AmplifySignOut />
+            </Card>
+          </Container>
+        </Jumbotron>
       </div>
-      <AmplifySignOut />
     </div>
   );
 }
