185f715711cdbb1712c4b744f37003658cb545ea
eccx400 <echeng@scu.edu>
2021-07-28 11:26:14 -0700

    Add working sentiment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import Amplify, { Predictions } from 'aws-amplify';
+import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 function App() {
+  const [response, setResponse] = useState("Please type some text");
+  const [textToInterpret, setTextToInterpret] = useState("write some text here to interpret");
+
+  function interpretFromPredictions() {
+    Predictions.interpret({
+      text: {
+        source: {
+          text: textToInterpret,
+        },
+        type: "ALL"
+      }
+    }).then(result => setResponse(JSON.stringify(result, null, 2)))
+      .catch(err => setResponse(JSON.stringify(err, null, 2)))
+  }
+
+  function setText(event) {
+    setTextToInterpret(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Text">
+      <div>
+        <h3>Text interpretation</h3>
+        <input value={textToInterpret} onChange={setText}></input>
+        <button onClick={interpretFromPredictions}>Interpret Sentiment</button>
+        <p>{response}</p>
+      </div>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
